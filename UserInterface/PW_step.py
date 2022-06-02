966a00315a037c3929b26c450541bb51b52f5443
jjacobson95 <jjacobso@uoregon.edu>
2022-06-02 16:49:26 -0700

    PW_step functional for mac and windows.

--- UserInterface/PW_step.py
+++ UserInterface/PW_step.py
@@ -9,10 +9,8 @@ import platform
 import pathlib
 import threading
 
-
-
-#Mac copy file functions
-def copy_a_file_mac(client, src,dst):
+#Copy file functions
+def copy_a_file(client, src,dst):
     name, dst = dst.split(':')
     container = client.containers.get(name)
     os.chdir(os.path.dirname(src))
@@ -26,40 +24,19 @@ def copy_a_file_mac(client, src,dst):
     container.put_archive(os.path.dirname(dst), data)
     os.remove((src + '.tar'))
 
-def copy_some_files_mac(client, src_list,dst):
+def copy_some_files(client, src_list,dst):
     for src in src_list:
         srcname = os.path.basename(src)
         dst = dst + srcname
-        copy_a_file_mac(client, src,dst)
+        copy_a_file(client, src,dst)
 
-#PC copy file functions
-def copy_a_file_PC(client, src,dst):
-    name, dst = dst.split(':')
-    container = client.containers.get(name)
-    srcname = os.path.basename(src).replace('"',"")
-    src = src.replace('"',"")
-    os.chdir(os.path.dirname(src))
-    tar = tarfile.open(src + '.tar', mode='w')
-    tar.add(srcname)
-    tar.close()
-    data = open(src + '.tar', 'rb').read()
-    container.put_archive(os.path.dirname(dst), data)
-    os.remove((src + '.tar'))
-
-def copy_some_files_PC(client, src_list,dst):
-    for src in src_list:
-        if src != "":
-            srcname = os.path.basename(src)
-            dst = dst + srcname
-            copy_a_file_PC(client, src,dst)
-
-def process(filepath, count,client,image,local_mem,command_list):
+def process(filepath,client,image,local_mem,command_list):
     file_path = str(filepath.absolute())
     cont_name = ("PW_container" + os.path.basename(file_path))
     client.containers.run(image,name=cont_name,volumes={local_mem: {'bind': '/III_mzML', 'mode': 'rw'}}, detach=True, tty=True)
     PW_container = client.containers.get(cont_name)
     copy_dst = cont_name + ":/III_mzML/"
-    copy_a_file_mac(client, file_path,copy_dst)
+    copy_a_file(client, file_path,copy_dst)
     command_list.pop()
     command_list.append(("/III_mzML/" + os.path.basename(file_path)))
     PW_container.exec_run(cmd=command_list)
@@ -74,21 +51,15 @@ def run_container(raw_file_folder):
 
     image = "jjacobson95/pw_test"    
     local_mem = os.getcwd() + "/III_mzML"
-
     client = docker.from_env()
-    
     command_list = ["wine", "/wineprefix64/drive_c/pwiz/msconvert", "--zlib", "-e",".mzMZ.gz","-o","/III_mzML", "placeholder"]
-
 
     threads = [] 
     count = 0
     for filepath in pathlib.Path(raw_file_folder).glob('**/*'):
-        threads.append(threading.Thread(target=process(filepath,count,client,image,local_mem,command_list)))  
+        threads.append(threading.Thread(target=process(filepath,client,image,local_mem,command_list)))  
         threads[count].start()
-        
         count +=1
-
-
     for t in threads:
         t.join()
 
