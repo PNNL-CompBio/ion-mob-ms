369e33c62303f3a0123f889e46dc15445a660b29
Jeremy Jacobson <85139244+jjacobson95@users.noreply.github.com>
2022-07-01 13:28:51 -0700

    Update pyinstaller_script.py

--- UserInterfaceV2/pyinstaller_script.py
+++ UserInterfaceV2/pyinstaller_script.py
@@ -6,20 +6,23 @@ def find(name, path):
         if name in files:
             return os.path.join(root, name)
 
-
+print(platform.system())
 if platform.system().upper() == "WINDOWS":
     Pyinstaller_location = find("pyinstaller.exe","/")
-    command = Pyinstaller_location + ' --collect-all sv_ttk --add-data="docs/*;docs" -F UI_V2.py'
+    command = Pyinstaller_location + ' -F UI_V2.py'
 if platform.system().upper() == "DARWIN":
-    command = 'pyinstaller --collect-all sv_ttk --add-data="docs/*:docs" -F UI_V2.py'
-
+    command = 'pyinstaller -F UI_V2.py'
+if platform.system().upper() == "LINUX":
+    command = 'pyinstaller -F UI_V2.py'
 os.system(command)
 
 if platform.system().upper() == "WINDOWS":
     os.system("rmdir /s /q build")
-    os.system("del UI_V2.spec")
+    #os.system("del UI_V2.spec")
 if platform.system().upper() == "DARWIN":
     os.system("rm -r build")
     os.system("rm UI_V2.spec")
-
+if platform.system().upper() == "LINUX":
+    os.system("rm -r build")
+    os.system("rm UI_V2.spec")
     
