05b65a0099bf8f13523664075d966173fdab7f30
Jacobson <jeremy.jacobson@pnnl.gov>
2023-02-14 11:02:04 -0800

    PW_step altered for testing more data

--- UserInterfaceV2/PW_step.py
+++ UserInterfaceV2/PW_step.py
@@ -14,13 +14,15 @@ import threading
 from multiprocessing import Pool
 import io
 import re
-
+from pathlib import Path
+import shutil
 
 #Set initial variables,
 #Determine local mem
 client = docker.from_env()
 image = "anubhav0fnu/proteowizard"    
-local_mem = os.getcwd() + "/III_mzML"
+local_mem = os.getcwd() + "/III_mzML_tmp"
+save_mem = os.getcwd() + "/III_mzML"
 # command_list = ["wine", "msconvert", "--zlib", "-e",".mzML.gz","-o","/III_mzML", "placeholder"]
 
 #This is the command that will be run in the container
@@ -40,11 +42,20 @@ def copy_a_file(client, src,dst):
     tar = tarfile.open(src + '.tar', mode='w')
     try:
         tar.add(srcname)
+        print(os.path.dirname(src)," debug_line 1")
+        time.sleep(1)
     finally:
         tar.close()
+        print(os.path.dirname(src)," debug_line 2")
+    time.sleep(1)
     data = open(src + '.tar', 'rb').read()
+    time.sleep(1)
+    print(os.path.dirname(src), " debug_line 3")
     container.put_archive(os.path.dirname(dst), data)
+    print(os.path.dirname(src), " debug_line 4")
+    time.sleep(1)
     os.remove((src + '.tar'))
+    print(os.path.dirname(src), " debug_line 5")
 
 def copy_some_files(client, src_list,dst):
     for src in src_list:
@@ -53,12 +64,31 @@ def copy_some_files(client, src_list,dst):
         copy_a_file(client, src,dst)
 
 
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+    
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    import stat
+    # Is the error an access error?
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
 
 def process(filepath):
-    global client,image,local_mem,command_list
+    global client,image,local_mem,command_list,save_mem
     file_path = str(filepath.absolute())
     cont_name = ("PW_container_" + os.path.basename(file_path))
     client.containers.run(image,name=cont_name,volumes={local_mem: {'bind': '/III_mzML', 'mode': 'rw'}}, detach=True, tty=True)
+    time.sleep(1)
     print("Container started: ", cont_name)
     PW_container = client.containers.get(cont_name)
     copy_dst = cont_name + ":/III_mzML/"
@@ -67,22 +97,42 @@ def process(filepath):
     command_list.pop()
     command_list.append(("/III_mzML/" + os.path.basename(file_path)))
     print("Proteowizard msconvert started in container: ",cont_name)
+    time.sleep(1)
     PW_container.exec_run(cmd=command_list)
     print("Proteowizard completed in container: ", cont_name)
-    
+    time.sleep(1)
+    ##
+    current_loc = (local_mem +"\\" + os.path.basename(file_path))
+    shutil.rmtree(current_loc, onerror=onerror)
+    # shutil.rmtree(current_loc)
+    #remove .d, add .mzml
+    print(os.path.basename(file_path), ": shutil")
+    current_loc = current_loc[:-2] + ".mzML"
+    mv_loc = (save_mem +"\\" + os.path.basename(file_path))
+    mv_loc = mv_loc[:-2] + ".mzML"
+    Path(current_loc).rename(mv_loc)
+    print(os.path.basename(file_path), ": rename")
+    ##
+    time.sleep(1)
     PW_container.stop()
     time.sleep(2)
     PW_container.remove()
+    time.sleep(1)
+    
+    
+    
 
 def run_container(raw_file_folder,exptype):
-    global client,image,local_mem,command_list
+    global client,image,local_mem,command_list,save_mem
     cur_dir = os.path.dirname(__file__)
     os.chdir(cur_dir)
-    local_mem = os.getcwd() + "/III_mzML"
+    local_mem = os.getcwd() + "/III_mzML_tmp"
+    save_mem = os.getcwd() + "/III_mzML"
     print("ProteoWizard Working Directory: ", local_mem)
     # print("directory is:", os.getcwd())
     # print("local mem is: ", local_mem)
     os.makedirs("./III_mzML", exist_ok = True)
+    os.makedirs("./III_mzML_tmp", exist_ok = True)
 
     # file_list = list(pathlib.Path(raw_file_folder).glob('*'))
     # print("TYPE:  ", type(file_list))
@@ -117,13 +167,14 @@ def run_container(raw_file_folder,exptype):
     #If this ever hits the cloud, "the limit does not exist!"
     #This generates subprocesses - each subprocess runs a container which runs one file.
     process_num = len(file_list)
-    if process_num > 10:
-        process_num = 10
+    if process_num > 1:
+        process_num = 1
 
     pool = Pool(processes=process_num)
     pool.map(process, file_list)
 
     pool.close()
     pool.join()
-    return local_mem
+    shutil.rmtree(local_mem)
+    return save_mem
 
