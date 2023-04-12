662de64ea263a47018db9d44867cb3280e3901f9
Jeremy Jacobson <85139244+jjacobson95@users.noreply.github.com>
2023-04-12 13:59:55 -0700

    Update MZ_hpc.py

--- singularity_dev/MZ_hpc.py
+++ singularity_dev/MZ_hpc.py
@@ -24,10 +24,7 @@ from datetime import datetime
 
 #Set initial variables,
 #Determine local mem
-
-# sed -i 's/REPLACE_THIS_LINE/        <parameter name="Raw data file names"><file>\/Work\/III_mzML\/Dorrestein_GnPS_P14_G10_msms_POS_01Dec20_Fiji_Infusion_Min50_MA-csum-Min20_1.mzML<\/file><\/parameter>/' /Work/MZmine_FeatureFinder-batch.xml
-# bash /MZmine-2.41.2/startMZmine_Linux.sh /Work/MZmine_FeatureFinder-batch.xml
-
+ 
 #add timestamps to print
 old_print = print
 def timestamped_print(*args, **kwargs):
@@ -45,58 +42,33 @@ def process(filepath):
     # options = ["--bind", "/vagrant/dev_dockerized/drf/backend/mzMLData:/home/vagrant"]
     myinstance = Client.instance('./mzmine_updated.sif', options=options)
     MZ_container = myinstance.name
-    
-    
-    print("A")
-    # Client.execute(myinstance,["mkdir","/home/vagrant/tmp"])
-    # Client.execute(myinstance,["cp","/Work/MZmine_FeatureFinder-batch.xml", "/home/vagrant/MZmine_FeatureFinder-batch.xml"])
-    print("filepath is ", filepath)
-    
-    # tmp ="7s/.*/" + """        <parameter name="Raw data file names"><file>\/Work\/III_mzML/""" + file_name + """<\/file><\/parameter>""" + "/"
+        
 #    command_list_0 = """Rscript /Work/R_PARSE_II.R"""
-
-
     command_list_0 = """Rscript /Work/R_PARSE_II.R ParseDTasRTmzML 1 /Work/III_mzML/""" + file_name
-   
-
-#    command_list_1 = """sed -i \'s/REPLACE_THIS_LINE/        <parameter name="Raw data file names"><file>\/Work\/III_mzML\/""" +file_name + """<\/file><\/parameter>/\' /Work/MZmine_FeatureFinder-batch.xml"""
     command_list_1 = """python MZmine_FeatureFinder_Modifier.py -n """ + file_name
     command_list_2 = """bash /MZmine-2.41.2/startMZmine_Linux.sh /Work/MZmine_FeatureFinder-batch.xml"""
     print("command_list_1:", command_list_1)
-    print("B")
-#    command_list_TEST = """touch /yolo"""
-#    command_list_TEST_TWO = """sh -c \'ls > /OUT.txt\'"""
-#    command_list_TEST_THREE = """head /Work/MZmine_FeatureFinder-batch.xml > /top"""
-#    Client.execute(myinstance,command_list_TEST, options=['--writable-tmpfs'],quiet=False)
-#    Client.execute(myinstance,command_list_TEST_TWO, options=['--writable-tmpfs'],quiet=False)
-#    Client.execute(myinstance,command_list_TEST_THREE, options=['--writable-tmpfs'],quiet=False)
-    
+
     shutil.copy(file_path, os.path.join(local_mem))
 
     Client.execute(myinstance,command_list_0, options=['--writable-tmpfs'],quiet=False)
-    print("C")
     Client.execute(myinstance,command_list_1, options=['--writable-tmpfs'],quiet=False)
     
-    # copy_dst = cont_name + ":/tmp/III_mzML/"
 #    shutil.copy(file_path, os.path.join(local_mem))
 
-    print("D")
     Client.execute(myinstance,command_list_2, options=['--writable-tmpfs'],quiet=False)
-    print("E")
-    # # #Client.execute(myinstance,["mv","/Work/IV_Features_csv/*.csv", "/home/vagrant"])
+
     print("Instance complete: ",myinstance,"   ",filepath)
     current_loc = (os.path.join(local_mem,os.path.basename(file_path)))
     current_loc = current_loc + "_c_dc_de.csv"
     mv_loc = (os.path.join(save_mem,os.path.basename(file_path)))
     mv_loc = mv_loc + "_c_dc_de.csv"
-    # os.chmod(current_loc,stat.S_IRWXG)
     Path(current_loc).rename(mv_loc)
 
     myinstance.stop()
     time.sleep(2)
 
 def run_container(mzML_data_folder):
-    #main container
     global local_mem, save_mem
     cur_dir = os.path.dirname(__file__)
     os.chdir(cur_dir)
@@ -142,124 +114,4 @@ def run_container(mzML_data_folder):
     pool.join()
     return local_mem
 
-    print("\n_________________\MZmine Complete.\n_________________\n")
-    # OUTPUT= tarfile.open("mzML_Zipfile","w")
-    # OUTPUT.add('//vagrant/dev_dockerized/drf/backend/TEST_pipeline/mzML_Files')
-    # OUTPUT.close()
 
-
-
-# steps: 
-# Main
-# 1) Open minio client. Copy PP data to location, untar it.
-# 2) For each file in PP, run a container.
-# Ind Container steps:
-# 3) mount container
-# 4) copy file to container. (or maybe not because it is mounted)
-# 5) run proteowizard
-# 6) close container
-#  Main
-# 7) return data to minio
-
-
-
-
-
-#Copy Functions: Copy from local Path to Path destination in a container.
-#These require files to be in .tar format (so these are converted here, then transfered).
-
-#Copy File functions
-# def copy_a_file(client, src,dst):
-#     name, dst = dst.split(':')
-#     container = client.containers.get(name)
-#     os.chdir(os.path.dirname(src))
-#     srcname = os.path.basename(src) 
-#     tar = tarfile.open(src + '.tar', mode='w')
-#     try:
-#         tar.add(srcname)
-#     finally:
-#         tar.close()
-#     data = open(src + '.tar', 'rb').read()
-#     container.put_archive(os.path.dirname(dst), data)
-#     os.remove((src + '.tar'))
-
-# def copy_some_files(client, src_list,dst):
-#     for src in src_list:
-#         srcname = os.path.basename(src)
-#         dst = dst + srcname
-#         copy_a_file(client, src,dst)
-
-
-
-# def process(filepath):
-#     global client,image,local_mem,command_list
-#     file_path = str(filepath.absolute())
-#     cont_name = ("PW_container_" + os.path.basename(file_path))
-#     client.containers.run(image,name=cont_name,volumes={local_mem: {'bind': '/III_mzML', 'mode': 'rw'}}, detach=True, tty=True)
-#     print("Container started: ", cont_name)
-#     PW_container = client.containers.get(cont_name)
-#     copy_dst = cont_name + ":/III_mzML/"
-#     copy_a_file(client, file_path,copy_dst)
-#     print("Files copied to container: ", cont_name)
-#     command_list.pop()
-#     command_list.append(("/III_mzML/" + os.path.basename(file_path)))
-#     print("Proteowizard msconvert started in container: ",cont_name)
-#     PW_container.exec_run(cmd=command_list)
-#     print("Proteowizard completed in container: ", cont_name)
-    
-#     PW_container.stop()
-#     time.sleep(2)
-#     PW_container.remove()
-
-# def run_container(raw_file_folder,exptype):
-#     global client,image,local_mem,command_list
-#     cur_dir = os.path.dirname(__file__)
-#     os.chdir(cur_dir)
-#     local_mem = os.getcwd() + "/III_mzML"
-#     print("ProteoWizard Working Directory: ", local_mem)
-#     # print("directory is:", os.getcwd())
-#     # print("local mem is: ", local_mem)
-#     os.makedirs("./III_mzML", exist_ok = True)
-
-    # file_list = list(pathlib.Path(raw_file_folder).glob('*'))
-    # print("TYPE:  ", type(file_list))
-
-    #test for mac
-
-    # file_list = list(pathlib.Path(raw_file_folder).glob('*'))
-
-    # # if singlefield...
-    # # if exptype == "Single":
-
-    # #If Single field data is not Singlefield (determined by suffix), this will do something.
-    # if exptype == "Single":
-    #     print('Note: Running Single-Field Workflow, any files that do not contain ms1 data will be removed.')
-    #     filtered_files =[]
-    #     for item in file_list:
-    #         ms_level = "no_suffix"
-    #         raw_item = "{}".format(item)
-    #         try:
-    #             print("raw_item is:", raw_item)
-    #             ms_level = re.search(r'\_([0-9]+)\.d', raw_item).group(1)
-    #             print("ms level of ", raw_item, " is ", ms_level)
-    #         except:
-    #             pass
-    #         if ms_level == "1" or ms_level == "no_suffix":
-    #             filtered_files.append(item)
-    #         else:
-    #             print("File not included due to ms level indicated by naming suffix (..._2-#).d,: ", item)
-    #     file_list = filtered_files
-
-    # #This limits containers to 10 at a time. This is important for running locally.
-    # #If this ever hits the cloud, "the limit does not exist!"
-    # #This generates subprocesses - each subprocess runs a container which runs one file.
-    # process_num = len(file_list)
-    # if process_num > 10:
-    #     process_num = 10
-
-    # pool = Pool(processes=process_num)
-    # pool.map(process, file_list)
-
-    # pool.close()
-    # pool.join()
-    # return local_mem
