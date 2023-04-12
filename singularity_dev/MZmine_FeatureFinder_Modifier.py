b91e80fa7d00da6cea905e16462c635e10d78ebf
Jeremy <jeremy.jacobson3402@gmail.com>
2023-04-12 10:16:45 -0700

    Intermediate script for MZ_hpc added

---
+++ singularity_dev/MZmine_FeatureFinder_Modifier.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.7
+
+import fileinput
+import argparse
+
+
+
+parser = argparse.ArgumentParser(description='Description of your program')
+parser.add_argument('-n','--filename', help='Prefilled Json File path', nargs='?', const='',default='')
+args = vars(parser.parse_args())
+
+FILE_NAME = args["filename"]
+WITH_LINE = """        <parameter name="Raw data file names"><file>\/Work\/III_mzML\/""" +FILE_NAME + """<\/file><\/parameter>"""
+
+filename = "/Work/MZmine_FeatureFinder-batch.xml"
+
+
+with fileinput.FileInput(filename, inplace = True) as f:
+    for line in f:
+        if("REPACE_THIS_LINE\n" == line):
+            print(WITH_LINE,end ='\n')
+        else:
+            print(line, end ='') 
+            
+            
