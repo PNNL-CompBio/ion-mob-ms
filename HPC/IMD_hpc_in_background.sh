001862da332fd038c462855567bacbd0da2c98b6
Jeremy Jacobson <85139244+jjacobson95@users.noreply.github.com>
2023-05-17 11:53:26 -0700

    Update IMD_hpc_in_background.sh

--- HPC/IMD_hpc_in_background.sh
+++ HPC/IMD_hpc_in_background.sh
@@ -1,8 +1,8 @@
 #!/bin/csh
-#SBATCH -A IMD_Dev
-#SBATCH -t 3-6
+#SBATCH -A Ion_Mob_Dashboard
+#SBATCH -t 1
 #SBATCH -N 1
-#SBATCH -n 48
+#SBATCH -n 2
 #SBATCH -J IMD_Dorrestein_stepped
 #SBATCH -o IMD_Dorrestein_stepped_output.txt
 #SBATCH -e IMD_Dorrestein_stepped_error.txt
@@ -12,10 +12,6 @@
 
 module load singularity
 module load python/3.7.0
-#echo "FIRST"
-#ls /rcfs/projects/IMD_IMD_Dev
-#echo "SECOND"
-#ls /rcfs/projects/IMD_IMD_Processing
 
 
-python3 CLI_hpc.py --json sample.json_testing
+python3 CLI_hpc.py --json sample.json
